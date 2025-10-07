39aa3ddc45948c77e36c2cbc1087d7cdf92f3b59
Steven Chan <steven.s.chan@oracle.com>
2025-10-07 22:58:32 +0100

    allow different maintenance_window_preference for infra and avmc

--- templates/aws-oci-adbd-quickstart/main.aws.tf
+++ templates/aws-oci-adbd-quickstart/main.aws.tf
@@ -33,7 +33,7 @@ module "exadata_infrastructure" {
   compute_count                          = var.compute_count
   storage_count                          = var.storage_count
   tags                                   = var.tags
-  maintenance_window_preference          = var.maintenance_window_preference
+  maintenance_window_preference          = var.infra_maintenance_window_preference
 }
 
 #Create odb network
@@ -73,5 +73,5 @@ module "autonomous_vm_cluster" {
   is_mtls_enabled_vm_cluster = var.is_mtls_enabled_vm_cluster
   scan_listener_port_non_tls = var.scan_listener_port_non_tls
   scan_listener_port_tls = var.scan_listener_port_tls
-  maintenance_window_preference = var.maintenance_window_preference
+  maintenance_window_preference = var.avmc_maintenance_window_preference
 }
