9033fda991be836ef36b881e25a76d988d469e2e
Steven Chan <steven.s.chan@oracle.com>
2025-10-08 17:44:34 +0100

    allow null for display_name, align with aws console

--- modules/aws-odb-peering/main.tf
+++ modules/aws-odb-peering/main.tf
@@ -1,7 +1,21 @@
+locals  {
+  # Determine if display_name is provided
+  is_display_name_provided = var.display_name != null && var.display_name != ""
+}
+
+# Generate a random suffix if display_name is not provided
+resource "random_string" "odbpc_suffix" {
+  count = local.is_display_name_provided ? 0 : 1
+  length           = 10
+  special          = false
+  upper            = false 
+}
+
 # Create the ODB Network Connection
 resource "aws_odb_network_peering_connection" "this" {
+  # Generate a name if not provided, aligning with AWS Console behavior
+  display_name    = local.is_display_name_provided ? var.display_name : "odbpcx-${random_string.odbpc_suffix[0].result}"
   odb_network_id  = var.odb_network_id
   peer_network_id = var.peer_network_id
-  display_name    = var.display_name
   tags            = var.tags
 }
