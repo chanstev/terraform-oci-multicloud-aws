8eed069603dc09f0b4c329fdf77b0c645f4447b5
Steven Chan <steven.s.chan@oracle.com>
2025-10-08 18:17:06 +0100

    update display_name of  ODB Peering Connection

--- modules/aws-odb-peering/main.tf
+++ modules/aws-odb-peering/main.tf
@@ -3,18 +3,10 @@ locals  {
   is_display_name_provided = var.display_name != null && var.display_name != ""
 }
 
-# Generate a random suffix if display_name is not provided
-resource "random_string" "odbpc_suffix" {
-  count = local.is_display_name_provided ? 0 : 1
-  length           = 10
-  special          = false
-  upper            = false 
-}
-
-# Create the ODB Network Connection
+# Create the ODB Peering Connection
 resource "aws_odb_network_peering_connection" "this" {
   # Generate a name if not provided, aligning with AWS Console behavior
-  display_name    = local.is_display_name_provided ? var.display_name : "odbpcx-${random_string.odbpc_suffix[0].result}"
+  display_name    = local.is_display_name_provided ? var.display_name : "${var.peer_network_id}-${var.odb_network_id}"
   odb_network_id  = var.odb_network_id
   peer_network_id = var.peer_network_id
   tags            = var.tags
