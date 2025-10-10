90a27eb29b160a14737e9d7b8b785640e0cc7cac
Steven Chan <steven.s.chan@oracle.com>
2025-10-10 14:39:06 +0100

    updated default name of odb peering

--- modules/aws-odb-peering/main.tf
+++ modules/aws-odb-peering/main.tf
@@ -6,7 +6,7 @@ locals  {
 # Create the ODB Peering Connection
 resource "aws_odb_network_peering_connection" "this" {
   # Generate a name if not provided, aligning with AWS Console behavior
-  display_name    = local.is_display_name_provided ? var.display_name : "${var.peer_network_id}-${var.odb_network_id}"
+  display_name    = local.is_display_name_provided ? var.display_name : "odbpcx-${var.peer_network_id}"
   odb_network_id  = var.odb_network_id
   peer_network_id = var.peer_network_id
   tags            = var.tags
